7226f6f6e8a5283728136b6fe12840eda12ded8a
Diwakar <you@example.com>
2024-12-17 15:04:55 +0530

    text

---
+++ js/if_else.js
@@ -0,0 +1,15 @@
+// var a="90";
+
+// if(isNaN(a)){
+//     alert('a is not a number')
+// }
+// else{
+//     if(a%2==0){
+//         alert(a+' is even number')
+//     }
+//     else if(a%2!=0){
+//         alert(a+' is odd number')
+//     }    
+// }
+
+
