44c5635e4c15c76883d1fd9986c938f50230566d
Diwakar <you@example.com>
2024-12-19 15:00:11 +0530

    looping

---
+++ js/for.js
@@ -0,0 +1,14 @@
+// for(let i=2;i<=100;i+=2){
+//     document.write(i)
+// }
+
+// for(i=3;i>=1;i--){
+//     document.write(i)
+// }
+
+// for(let j=1;j<=3;j++){
+//     for(let i=1;i<=j;i++){
+//         document.write('*')
+//     }
+//     document.write('<br>')
+// }
